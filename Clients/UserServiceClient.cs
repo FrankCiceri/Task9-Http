0f0de2268052b0c2b7710b7c119bc669f75e065f
FrankCiceri <frankanthonyc00@gmail.com>
2023-05-19 16:02:49 -0500

    Finished UserService

--- Clients/UserServiceClient.cs
+++ Clients/UserServiceClient.cs
@@ -28,13 +28,45 @@ namespace Task9.Clients
             return response;
         }
 
+        public async Task<string> GetUserStatus(int userId)
+        {
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{_baseUrl}/UserManagement/GetUserStatus?userId={userId}"),
+            };
+
+            HttpResponseMessage response = await _client.SendAsync(request);
+
+            return await response.Content.ReadAsStringAsync();
+
+        }
+
+
+        public async Task<HttpStatusCode> SetUserStatus(int userId, bool newStatus)
+        {
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Put,
+                RequestUri = new Uri($"{_baseUrl}/UserManagement/SetUserStatus?userId={userId}&newStatus={newStatus}"),
+            };
+
+            HttpResponseMessage response = await _client.SendAsync(request);
+
+            return response.StatusCode;
+
+        }
+
+
+
         public async Task<HttpStatusCode> DeleteUser(int userId) {
 
             var request = new HttpRequestMessage
             {
-                Method = HttpMethod.Post,
-                RequestUri = new Uri($"{_baseUrl}/Register/DeleteUser"),
-                Content = new StringContent(JsonConvert.SerializeObject(userId), Encoding.UTF8, "application/json"),
+                Method = HttpMethod.Delete,
+                RequestUri = new Uri($"{_baseUrl}/Register/DeleteUser?userId={userId}"),
             };
 
             HttpResponseMessage response = await _client.SendAsync(request);
--- UserServiceTests.cs
+++ UserServiceTests.cs
@@ -16,18 +16,18 @@ namespace Task9
 
         [SetUp]
         public void Setup()
-        { 
+        {
 
         }
         //2      
-        [TestCase(null, null)]        
+        [TestCase(null, null)]
         [TestCase("", null)]
         public async Task RegisterUser_Null_StatusCodeInternalServerError(string firstName, string lastName)
         {
             //Precondition            
 
             UserServiceRegisterUserRequest requestBody = new UserServiceRegisterUserRequest();
-            requestBody.SetBody(firstName,lastName);
+            requestBody.SetBody(firstName, lastName);
 
             //Action
 
@@ -35,13 +35,13 @@ namespace Task9
 
             //Assert
 
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.InternalServerError));   
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.InternalServerError));
         }
 
         //1,3,4,5,6,7
         [TestCase(0)]//Length 0 means Empty String
         [TestCase(1)]
-        [TestCase(100)] 
+        [TestCase(100)]
         [TestCase(500)]
         public async Task RegisterUser_MultipleCharactersVariableLength_StatusCodeOK(int length)
         {
@@ -57,6 +57,7 @@ namespace Task9
 
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
         }
+
         //8,9
         [TestCase(false)]
         [TestCase(true)]
@@ -75,18 +76,93 @@ namespace Task9
             if (enableDelete)
                 await _userService.DeleteUser(id1);
 
-            HttpResponseMessage response2 = await _userService.RegisterUser(requestBody);            
+            HttpResponseMessage response2 = await _userService.RegisterUser(requestBody);
             int id2 = Int32.Parse(await response2.Content.ReadAsStringAsync());
 
             //Assert
-            
-            Assert.That(id2 - id1, Is.GreaterThan(0));
+
+            Assert.That(id2 - id1, Is.EqualTo(1));
         }
 
-        //[test]
-        //public async Task DeleteUser_NotActive_StatusCodeOK() {
-        
-        //}
+        //11,20
+        [Test]
+        public async Task DeleteGetUser_NotActive_StatusCodeOK() {
+
+            UserServiceRegisterUserRequest requestBody = new UserServiceRegisterUserRequest();
+            requestBody.SetBody(10);
+
+            HttpResponseMessage response = await _userService.RegisterUser(requestBody);
+            int id = Int32.Parse(await response.Content.ReadAsStringAsync());
+
+            string getStatus = await _userService.GetUserStatus(id);
+
+            var statusCode = await _userService.DeleteUser(id);
+
+            Assert.Multiple(() => {
+                Assert.That(getStatus, Is.EqualTo("false"));
+                Assert.That(statusCode, Is.EqualTo(HttpStatusCode.OK));
+            });
+
+
+        }
+        //10,14,21
+        [Test]
+        public async Task DeleteGetSetUser_NotExisting_StatusCodeInternalServerError()
+        {
+            UserServiceRegisterUserRequest requestBody = new UserServiceRegisterUserRequest();
+            requestBody.SetBody(10);
+
+            HttpResponseMessage response = await _userService.RegisterUser(requestBody);
+            int id = Int32.Parse(await response.Content.ReadAsStringAsync());
+            await _userService.DeleteUser(id);
+
+
+            string getStatus = await _userService.GetUserStatus(id);
+            var setStatusCode = await _userService.SetUserStatus(id, true);
+            var deleteStatusCode = await _userService.DeleteUser(id);
+
+
+            Assert.Multiple(() => {
+                Assert.That(getStatus, Is.EqualTo("Sequence contains no elements"));
+                Assert.That(setStatusCode, Is.EqualTo(HttpStatusCode.InternalServerError));
+                Assert.That(deleteStatusCode, Is.EqualTo(HttpStatusCode.InternalServerError));
+            });
+
+        }
+        //12,13,15,16,17,18,19
+        [TestCase(new [] {false, true})]
+        [TestCase(new[] { true, false, true, true})]
+        public async Task SetGetUser_StatusChange_StatusCodeInternalServerError(bool[] changes)
+        {
+            //Pre-Condition
+
+            UserServiceRegisterUserRequest requestBody = new UserServiceRegisterUserRequest();
+            requestBody.SetBody(10);
+
+            HttpResponseMessage response = await _userService.RegisterUser(requestBody);
+            int id = Int32.Parse(await response.Content.ReadAsStringAsync());
+
+            //Action
+
+            var setStatusCode = await _userService.SetUserStatus(id, false);
+
+            foreach ( var change in changes){
+
+                setStatusCode = await _userService.SetUserStatus(id, change);
+
+            }
+
+            string getStatus = await _userService.GetUserStatus(id);
+            Console.WriteLine(true);
+
+            //Assert
+
+            Assert.Multiple(() => {
+                Assert.That(getStatus, Is.EqualTo($"{changes.Last()}".ToLower()));
+                Assert.That(setStatusCode, Is.EqualTo(HttpStatusCode.OK));
+            });
+
+        }
 
     }
 }
